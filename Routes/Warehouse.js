bfaa55e4dc38990ee4baabaddd1bef5d2ba6edb7
Zenhar-Prodipto <zenhar.mz@gmail.com>
2021-09-07 23:40:50 +0600

    Final touches

--- Routes/Warehouse.js
+++ Routes/Warehouse.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-//Imports from Authorization Controller
+//Imports from Authorization Controllers
 const {
   requireSignin,
   isAdmin,
